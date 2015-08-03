847e0f4250bdb1e46a2e23a6ddfdfd46dade7bfd
Przemysław Janowski <przemyslaw.janowski@outlook.com>
2015-08-04 00:44:50 +0200

    simplified CLI even more

--- lib/vahana.rb
+++ lib/vahana.rb
@@ -10,10 +10,6 @@ require "vahana/single_record"
 require "vahana/mongo"
 require "vahana/redis"
 require "vahana/neo4j"
-require "vahana/cli/redis"
-require "vahana/cli/mongo"
-require "vahana/cli/neo4j"
-require "vahana/cli/cassandra"
 require "vahana/cli/application"
 
 
--- lib/vahana/cli/application.rb
+++ lib/vahana/cli/application.rb
@@ -2,22 +2,38 @@ module Vahana
   module Cli
     class Application < Thor
 
-      desc 'hello NAME', 'Display greeting with given NAME'
-      def hello(name)
-        puts "Hello #{name}"
+      desc 'prepare', 'Drops data from all databases. Use with care!'
+      def prepare
+        Vahana::Redis.new.drop
+        Vahana::Mongo.new.drop
+        Vahana::Redis.new.drop
+        Vahana::Redis.new.drop
       end
 
-      desc 'redis TARGET', 'Migrate data from Redis to TARGET database'
-      subcommand 'redis', Vahana::Cli::Redis
+      desc 'seed TARGET', 'Seeds TARGET database with test data.'
+      def seed(database)
+        begin
+          ['Vahana', database.capitalize].inject(Object) {|o,c| o.const_get c}.new.seed
+        rescue NameError
+          say "ERROR: Provided target is not supported. Compatible databases are: redis, mongo, cassandra, neo4j."
+        end
+      end
 
-      desc 'mongo TARGET', 'Migrate data from MongoDB to TARGET database'
-      subcommand 'mongo', Vahana::Cli::Mongo
+      desc 'migrate SOURCE TARGET', 'Copies all data from SOURCE to TARGET database.'
+      def migrate(source, target)
+        begin
+          say "Migrating all data from #{source.capitalize} to #{target.capitalize}..."
+          s = ['Vahana', source.capitalize].inject(Object) {|o,c| o.const_get c}.new
+          t = ['Vahana', target.capitalize].inject(Object) {|o,c| o.const_get c}.new
+          s.transfer_all_to(t)
+          say 'Done!'
+        rescue NoMethodError
+          say "WARNING: This migration is not implemented yet."
+        rescue NameError
+          say "ERROR: Provided source or target is not supported. Compatible databases are: redis, mongo, cassandra, neo4j."
+        end
+      end
 
-      desc 'neo4j TARGET', 'Migrate data from Neo4j to TARGET database'
-      subcommand 'neo4j', Vahana::Cli::Neo4j
-
-      desc 'cassandra TARGET', 'Migrate data from Cassandra to TARGET database'
-      subcommand 'cassandra', Vahana::Cli::Cassandra
     end
   end
 end
